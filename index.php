2224f838efa70eb1a595b46e6115d18502f491e1
Chris Coppola <mynamereallysux@gmail.com>
2017-06-22 17:18:00 -0400

    Fixed git info

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ $isMainRepo = $repositoryUrl == $mainRepo;
   <div class="row">
     <div class="col-xs-12">
       <?php if($repositoryUrl && $branch){ ?>
-        <div class="alert <?php echo $isMainRepo || $isMaster ? 'alert-danger' : 'alert-success' ?>" role="alert">
+        <div class="alert <?php echo $isMainRepo && $isMaster ? 'alert-danger' : 'alert-success' ?>" role="alert">
           <p>
             <strong>Current Respository: </strong><a href="<?php echo $repositoryUrl; ?>"><?php echo $repositoryUrl; ?></a><br/>
             <strong>Current Branch: </strong><?php echo $branch; ?>
